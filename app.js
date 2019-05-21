697b4acbe808bb7ce596c5d419b83423e06fc0fb
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-21 11:03:31 +0530

    make it better2

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.get('/',(req,res) => {
 
 });
 app.listen((process.env.PORT || 5000),() =>{
-    console.log('app now listening to 5000');
+    console.log('app is now listening to 5000');
 });
