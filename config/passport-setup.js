7c43a757e7fd72d7d57870070f571f45910ff89e
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-24 15:49:08 +0530

    make it better

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,8 +6,8 @@ const User =  require('../models/user-model');
 passport.use(
     new GoogleStrategy({
         //options for the google strategy
-        clientID: 'keys.google.clientID',
-        clientSecret: 'keys.google.clientSecret',
+        clientID: '414832118642-mb7fuhl7v1e6vhqta6d8dm3id4tam6pq.apps.googleusercontent.com',
+        clientSecret: 'RDTgSn28uZeReD11K7ebU1fL',
         callbackURL: 'https://oauth-playlist-2015.herokuapp.com/auth/google/redirect'
     },() =>{
         //passport callback 
