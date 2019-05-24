983e1e50e687af6da0d4f521276280984882b9f2
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-24 17:16:06 +0530

    make it better

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const authRoutes = require('./routes/auth-routes'); //importing routes
 
 const passportSetup = require('./config/passport-setup');
 
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-//const keys = require('./config/keys');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express(); //instantiate app.js with express?
 //basically including express in app.js?
@@ -14,9 +16,18 @@ const app = express(); //instantiate app.js with express?
 //set up view engine
 app.set('view engine', 'ejs');
 
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 *1000,
+    keys: ['iamawesome']
+}))
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect to mongodb
-//mongoose.connect("mongodb://oauth-playlist-2015.herokuapp.com");
-//console.log('connected');
+mongoose.connect("mongodb://oauth-playlist-2015.herokuapp.com");
+console.log('connected');
 
 //set up routes
 app.use('/auth',authRoutes); //when the user goes to /auth/(something) /auth/login or logout or google
