4a4289836052f5c53dc87ad9c041739bf4114767
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-25 15:26:50 +0530

    make it better

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,42 +1,41 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
-const User =  require('../models/user-model');
+const User = require('../models/user-model');
 
-passport.serializeUser((user, done)=>{
+passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser((id, done)=>{
-    User.findById(id).then((user)=>{
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
         done(null, user);
     });
 });
 
 passport.use(
     new GoogleStrategy({
-        //options for the google strategy
-        clientID: '414832118642-mb7fuhl7v1e6vhqta6d8dm3id4tam6pq.apps.googleusercontent.com',
-        clientSecret: 'RDTgSn28uZeReD11K7ebU1fL',
-        callbackURL: 'https://oauth-playlist-2015.herokuapp.com/auth/google/redirect'
-    },(acessToken, refreshToken, profile, done) =>{
-        //check if already exists
-        User.findOne({googleId: profile.id}).then((currentUser)=>{
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
             if(currentUser){
-                //already exists
+                // already have this user
+                console.log('user is: ', currentUser);
                 done(null, currentUser);
-            }else{
+            } else {
+                // if not, create user in our db
                 new User({
-                    username: profile.displayName,
                     googleId: profile.id,
-                    thumbnail: profile._json.image.url
-                    //email
-                }).save().then((newUser)=>{
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
                     done(null, newUser);
                 });
             }
         });
-        
     })
-)
-
+);
