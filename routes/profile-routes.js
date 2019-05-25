c5a8be6ae9960b68967742fc376bfab3732465d2
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-25 13:50:50 +0530

    make it better

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,16 +1,7 @@
 const router = require('express').Router();
 
-const authCheck = (req, res, next)=>{
-    if(!req.user){
-        //if user is not logged in 
-        res.redirect('/auth/login');
-    } else{
-        //if logged in
-        next();
-    }
-};
-router.get('/', authCheck, (req, res)=>{
-    res.send(req.user.username);
+router.get('/',(req, res)=>{
+    res.send('you are logged in' + req.user.username);
 });
 
 module.exports = router;
