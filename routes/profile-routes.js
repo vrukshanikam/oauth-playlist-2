8eee7144c86d0fa58c68c7cada9bf853f4c4d943
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-25 13:36:30 +0530

    m

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -10,7 +10,7 @@ const authCheck = (req, res, next)=>{
     }
 };
 router.get('/', authCheck, (req, res)=>{
-    res.render('profile', {user: req.user});
+    res.render(req.user.username);
 });
 
 module.exports = router;
