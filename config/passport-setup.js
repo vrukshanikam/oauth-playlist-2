ec0eb83e9d7f75bc5988cca3ff3f6793e17026ca
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-25 15:29:34 +0530

    make it better

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -18,7 +18,7 @@ passport.use(
         // options for google strategy
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
-        callbackURL: '/auth/google/redirect'
+        callbackURL: 'http://oauth-playlist-2015.herokuapp.com/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // check if user already exists in our own db
         User.findOne({googleId: profile.id}).then((currentUser) => {
