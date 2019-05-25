cfbc3bf0741e9842e0be0acd068f8d9179b8663c
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-25 13:11:10 +0530

    make it better

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -17,7 +17,7 @@ router.get('/google', passport.authenticate('google',{
 }));
 
 router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
-    res.send('CALLBACK URI');
+    res.send('req.user');
 });
 
 module.exports = router;
