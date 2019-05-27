be27e2dac9c1cda12907a43529b0d84df542828f
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-27 09:29:32 +0530

    make it better

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -14,7 +14,7 @@ router.get('/logout', (req,res) =>{
 });
 
 router.get('/google', passport.authenticate('google',{
-    scope: ['profile']
+    scope: ['profile', 'email']
 }));
 
 router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
