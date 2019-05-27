65ac01e3e96dfb23a3123ad5cfbea1946c1608e4
Vruksha Nikam <honeynikam3141@gmail.com>
2019-05-27 14:37:46 +0530

    Add cool face API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const morgan = require('morgan');
 const authRoutes = require('./routes/auth-routes'); 
 const profileRoutes = require('./routes/profile-routes'); 
 
